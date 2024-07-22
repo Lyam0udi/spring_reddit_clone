738be6c83178f36437e27581f800ca85b7229804
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-07-22 16:48:43 +0100

    Adding security config

---
+++ src/main/java/com/example/spring_reddit_clone/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.spring_reddit_clone.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll() // Added permitAll() to allow access to specified endpoints
+                .anyRequest().authenticated();
+    }
+}
